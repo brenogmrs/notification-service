77e39ad9f42d4e23d5f82f84f2e38dc514c4c7c2
brenogmrs <brenoaraujo@outlook.com.br>
2023-01-19 23:26:24 -0300

    feat: find by id to abstract class

--- src/application/repositories/notification-repository.ts
+++ src/application/repositories/notification-repository.ts
@@ -2,4 +2,5 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsReporitory {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
 }
