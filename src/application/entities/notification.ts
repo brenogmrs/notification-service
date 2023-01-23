fea685c80e6794804d8ce18ea3896d19c4e0b01d
brenogmrs <brenoaraujo@outlook.com.br>
2023-01-23 13:03:05 -0300

    chore: add migration for the canceledAt field

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -55,6 +56,14 @@ export class Notification {
 
   public get readAt(): ReadAt {
     return this.props.readAt;
+  }
+
+  public get caceledAt(): ReadAt {
+    return this.props.canceledAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
   }
 
   public get createdAt(): Date {
