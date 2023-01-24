d7af0d588eb042d30d48b8b38c3840331f0db15c
brenogmrs <brenoaraujo@outlook.com.br>
2023-01-24 11:36:09 -0300

    chore: fix error import

--- src/application/use-cases/count-recipient-notifications.ts
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { NotificationsReporitory } from '../repositories/notification-repository';
 
 interface CountRecipientNotificationRequest {
